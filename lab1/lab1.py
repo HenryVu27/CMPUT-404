ddcd8bee4091f6a96419a3650ece99317c0fd1f7
Henry Vu <henryvu@Henrys-MacBook-Pro.local>
2023-09-18 17:07:02 -0600

    changed url

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,5 +3,5 @@ print("requests version:", requests.__version__)
 print("GET Google homepage:", requests.get("http://google.com/"))
 
 
-lab1_code = requests.get("https://github.com/HenryVu27/CMPUT-404-lab1/blob/main/lab1/lab1.py")
+lab1_code = requests.get("https://raw.githubusercontent.com/HenryVu27/CMPUT-404-lab1/main/lab1/lab1.py")
 print("Printing source code:", lab1_code.text)

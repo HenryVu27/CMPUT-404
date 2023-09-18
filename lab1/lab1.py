6f4fe8143288033f6460a9924a65f41e0676dbd8
Henry Vu <henryvu@Henrys-MacBook-Pro.local>
2023-09-18 13:33:53 -0600

    modify python file

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,3 +1,7 @@
 import requests
 print("requests version:", requests.__version__)
 print("GET Google homepage:", requests.get("http://google.com/"))
+
+
+lab1_code = requests.get("https://github.com/HenryVu27/CMPUT-404-lab1/blob/main/lab1/lab1.py")
+print("Printing source code:", lab1_code.text)
